a02e0c5bf021e86f2f56aa8f8fd43e15fca380c7
ritushreedas2002 <ritushreedas20027@gmail.com>
2024-11-15 01:42:56 +0530

    removed all the commented code block

--- src/app/Pages/signup/page.tsx
+++ src/app/Pages/signup/page.tsx
@@ -1,38 +1,3 @@
-// "use client"
-// import { useRouter } from 'next/navigation';
-// import SignUpForm from '@/app/components/User/SignUpForm';
-// import { registerUser } from '../../lib/api';
-// import { useState } from 'react';
-
-// const SignUp: React.FC = () => {
-//   const [error, setError] = useState('');
-//   const router = useRouter();
-
-//   const handleSignUp = async (username: string, email: string, password: string) => {
-//     try {
-//        await registerUser(username, email, password);
-
-//       router.push('/Pages/login');
-//     } catch (err) {
-//       if (err instanceof Error) {
-//         setError(err.message);
-//       } else {
-//         setError('An unknown error occurred');
-//       }
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <h1>Sign Up</h1>
-//       <SignUpForm onSubmit={handleSignUp} error={error} />
-//     </div>
-//   );
-// };
-
-// export default SignUp;
-
-
 
 "use client"
 
